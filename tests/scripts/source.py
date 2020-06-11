2b54c36e4961f3569950f1e79fcd4bcf0037110e
Luki <tahiraj.f@gmail.com>
2020-06-11 02:19:00 +0200

    Initial commit

---
+++ tests/scripts/source.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+
+cmd = [sys.executable, 'speedtest.py', '--source', '127.0.0.1']
+
+p = subprocess.Popen(
+    cmd,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+
+stdout, stderr = p.communicate()
+
+if p.returncode != 1:
+    raise SystemExit('%s did not fail with exit code 1' % ' '.join(cmd))
+
+if 'Invalid argument'.encode() not in stderr:
+    raise SystemExit(
+        '"Invalid argument" not found in stderr:\n%s' % stderr.decode()
+    )

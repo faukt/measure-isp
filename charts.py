20113a06a3d3c898c9c4699f310f86494bb2d10b
Luki <tahiraj.f@gmail.com>
2020-06-13 00:18:37 +0200

    do not stop creation of CSV due to invalid lines

--- charts.py
+++ charts.py
@@ -1,6 +1,5 @@
 import glob
 import plotly.express as px
-import plotly.graph_objects as go
 import pandas as pd
 import re
 import json
@@ -15,12 +14,17 @@ def toCSV(filename):
         with open(filename, 'r') as filePointer:
             with open(filename + '.csv', 'w+') as filePointerCSV:
                 filePointerCSV.write(CSVHEADER)
+                lineNo = 0
                 for line in filePointer:
-                    dateTime = re.search('#(.*)#', line)
-                    lineJsonS = line[(dateTime.span()[1]+1):(len(line))]
-                    lineJson = json.loads(lineJsonS)
-                    csv_line = '"{}",{},{},{}\n'.format(dateTime.group(1), round(lineJson['download']/1000000, 2), round(lineJson['upload']/1000000, 2), round(lineJson['ping'], 2))
-                    filePointerCSV.write(csv_line)
+                    try:
+                        lineNo += 1
+                        dateTime = re.search('#(.*)#', line)
+                        lineJsonS = line[(dateTime.span()[1]+1):(len(line))]
+                        lineJson = json.loads(lineJsonS)
+                        csv_line = '"{}",{},{},{}\n'.format(dateTime.group(1), round(lineJson['download']/1000000, 2), round(lineJson['upload']/1000000, 2), round(lineJson['ping'], 2))
+                        filePointerCSV.write(csv_line)
+                    except Exception as Ex:
+                        print('Excetion during line-parsing to CSV! LineNo. {} Ex: {}'.format(lineNo, str(Ex)))
     except Exception as Ex:
         print('Excetion during convertion to CSV! Ex: %s' % str(Ex))
 
